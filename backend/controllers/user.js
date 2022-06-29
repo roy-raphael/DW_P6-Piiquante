91be0b5506d4cc6704e23a00989ce8dbdd3dff71
Raphael Roy <roy.raphael.33@gmail.com>
2022-06-29 10:46:31 +0200

    docs: add comments for the user routes (to generate OpenAPI doc)

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -2,6 +2,43 @@ import User from '../models/user.js';
 import bcrypt from 'bcrypt';
 import jwt from 'jsonwebtoken';
 
+/*
+ * @oas [post] /api/auth/signup
+ * tags: ["auth"]
+ * summary: Creation of a new user
+ * description: Hachage du mot de passe de l'utilisateur, ajout de l'utilisateur à la base de données
+ * requestBody:
+ *  required: true
+ *  content:
+ *    application/json:
+ *      schema:
+ *        $ref: "#/components/schemas/user"
+ * responses:
+ *  "201":
+ *    description: "OK"
+ *    content:
+ *      application/json:
+ *        schema:
+ *          type: "string"
+ *          description: Message de création d'utilisateur
+ *        example: Utilisateur créé
+ *  "400":
+ *    description: TODO
+ *    content:
+ *      application/json:
+ *        schema:
+ *          $ref: "#/components/schemas/errorMessage"
+ *        example: TODO
+ *  "500":
+ *    description: TODO
+ *    content:
+ *      application/json:
+ *        schema:
+ *          $ref: "#/components/schemas/errorMessage"
+ *        example: TODO
+ */
+// IN : { email: string, password: string }
+// OUT: { message: string }
 export function signup(req, res, next) {
     bcrypt.hash(req.body.password, 10)
     .then(hash => {
@@ -16,6 +53,52 @@ export function signup(req, res, next) {
     .catch(error => res.status(500).json({ error }));
 }
 
+/*
+ * @oas [post] /api/auth/login
+ * tags: ["auth"]
+ * summary: Connexion of a user
+ * description: >
+ *   Vérification des informations d'identification de l'utilisateur, renvoie l _id de
+ *   l'utilisateur depuis la base de données et un token web JSON signé
+ *   (contenant également l'_id de l'utilisateur).
+ * requestBody:
+ *  required: true
+ *  content:
+ *    application/json:
+ *      schema:
+ *        $ref: "#/components/schemas/user"
+ * responses:
+ *  "200":
+ *    description: "OK"
+ *    content:
+ *      application/json:
+ *        schema:
+ *          type: "object"
+ *          properties:
+ *            userId:
+ *              type: string
+ *              description: ID de l'utilisateur (depuis la base de données)
+ *            token:
+ *              type: string
+ *              description: token web JSON signé (contenant également l'ID de l'utilisateur)
+ *        example: TODO
+ *  "401":
+ *    description: TODO
+ *    content:
+ *      application/json:
+ *        schema:
+ *          $ref: "#/components/schemas/errorMessage"
+ *        example: TODO
+ *  "500":
+ *    description: TODO
+ *    content:
+ *      application/json:
+ *        schema:
+ *          $ref: "#/components/schemas/errorMessage"
+ *        example: TODO
+ */
+// IN : { email: string, password: string }
+// OUT: { userId: string, token: string }
 export function login(req, res, next) {
     User.findOne({ email: req.body.email })
     .then(user => {
--- backend/models/user.js
+++ backend/models/user.js
@@ -2,13 +2,15 @@ import mongoose from 'mongoose';
 import uniqueValidator from 'mongoose-unique-validator';
 
 const userSchema = new mongoose.Schema({
-  email: { type: String, required: true, unique: true },
-  password: { type: String, required: true }
-}, {
-  timestamps: {
-    createdAt: 'created_at',
-    updatedAt: 'updated_at'
-  }
+    // e-mail adress of the user [unique]
+    email: { type: String, required: true, unique: true },
+    // user hashed password
+    password: { type: String, required: true }
+    }, {
+    timestamps: {
+        createdAt: 'created_at',
+        updatedAt: 'updated_at'
+    }
 });
 
 userSchema.plugin(uniqueValidator);
