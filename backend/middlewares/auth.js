2a96907bd8f42189dee90b88cb86969972e94a3a
Raphael Roy <roy.raphael.33@gmail.com>
2022-06-29 09:43:31 +0200

    feat: add authentication middleware

---
+++ backend/middlewares/auth.js
@@ -0,0 +1,19 @@
+import jwt from 'jsonwebtoken';
+
+export default function authorise(req, res, next) {
+    try {
+        const token = req.headers.authorization.split(' ')[1];
+        const decodedToken = jwt.verify(token, 'RANDOM_TOKEN_SECRET');
+        const userId = decodedToken.userId;
+        req.auth = { userId };
+        if (req.body.userId && req.body.userId !== userId) {
+            throw 'Invalid user ID';
+        } else {
+            next();
+        }
+    } catch {
+        res.status(401).json({
+            error: new Error('Invalid request!')
+        });
+    }
+}
