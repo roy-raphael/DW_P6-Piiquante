546c03946c32dfe97dcfd9362849f852fe6cb54d
Raphael Roy <roy.raphael.33@gmail.com>
2022-06-30 17:45:10 +0200

    feat: update multer middleware to save images with a timestamp formated for humans

--- backend/middlewares/multer-config.js
+++ backend/middlewares/multer-config.js
@@ -11,9 +11,13 @@ const storage = multer.diskStorage({
         callback(null, 'images');
     },
     filename: (req, file, callback) => {
-        const name = file.originalname.split(' ').join('_');
+        const fullName = file.originalname.split(' ').join('_');
+        const name = fullName.split('.').slice(0, -1).join('.');
         const extension = MIME_TYPES[file.mimetype];
-        callback(null, name + Date.now() + '.' + extension);
+        // Get current date at the format : YYYYMMdd_hhmmss (toISOString gives YYYY-MM-ddThh:mm:ss.zzzZ)
+        const dateReg = /[-:]/g;
+        var date = new Date(Date.now()).toISOString().replace(dateReg, '').replace('T', '_').split('.').slice(0, -1).join();
+        callback(null, name + '_' + date + '.' + extension);
     }
 });
 
