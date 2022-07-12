9cf05985eb372f4a4f382477ccfd1e0d85ddd7b7
Raphael Roy <roy.raphael.33@gmail.com>
2022-07-12 17:14:32 +0200

    feat: add mongoose schema validation

--- backend/models/sauce.js
+++ backend/models/sauce.js
@@ -14,11 +14,11 @@ const sauceSchema = new mongoose.Schema({
     // URL of the sauce image uploaded by the user
     imageUrl: { type: String, required: true },
     // number between 1 and 10 describing the sauce
-    heat: { type: Number, required: true },
+    heat: { type: Number, required: true, min: [1, 'Must be at least 1, got {VALUE}'], max: [10, 'Must be at most 10, got {VALUE}'] },
     // number of users liking the sauce
-    likes: { type: Number, required: true },
+    likes: { type: Number, required: true, min: [0, 'Must be at least 0, got {VALUE}'] },
     // number of users disliking the sauce
-    dislikes: { type: Number, required: true },
+    dislikes: { type: Number, required: true, min: [0, 'Must be at least 0, got {VALUE}'] },
     // IDs array of users who liked the sauce
     usersLiked: { type: [String], required: true },
     // IDs array of users who disliked the sauce
