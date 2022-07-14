53c2cf8b500615b2bbaa665ce879021c729d71d7
Raphael Roy <roy.raphael.33@gmail.com>
2022-07-14 11:34:02 +0200

    feat: add middleware to resize image if too big

---
+++ backend/middlewares/image-resize.js
@@ -0,0 +1,78 @@
+import fs from 'fs';
+import sharp from 'sharp';
+
+const SAUCES_IMAGES_SAVE_PATH = 'images';
+const SAUCES_IMAGES_MAX_LENGTH = 500;
+
+async function resizeImage(req, res, next) {
+    const tmpFileRelativePath = `${SAUCES_IMAGES_SAVE_PATH}/resized-${req.file.filename}`;
+    const fileRelativePath = `${SAUCES_IMAGES_SAVE_PATH}/${req.file.filename}`;
+
+    let image = sharp(req.file.path);
+    let meta = await image.metadata();
+
+    if (meta.width > SAUCES_IMAGES_MAX_LENGTH && meta.height > SAUCES_IMAGES_MAX_LENGTH) {
+        // Resize the image
+        try {
+            const format = meta.format;
+            image.rotate() // rotates the image using the original orientation in the EXIF data
+            image.resize(SAUCES_IMAGES_MAX_LENGTH, SAUCES_IMAGES_MAX_LENGTH, {fit: 'outside'});
+            if (format === "jpg" || format === "jpeg") {
+                image.jpeg({ quality: 90 });
+            } else if (format === "png") {
+                image.png({ compressionLevel: 9, adaptiveFiltering: true });
+            }
+            await image.toFile(tmpFileRelativePath);
+        } catch (error) {
+            console.error(error);
+            return;
+        }
+        // Check the new file size
+        let newSize = 0;
+        try {
+            newSize = fs.statSync(tmpFileRelativePath).size;
+        } catch (error) {
+            console.log(error);
+        }
+        // Keep only one file
+        if (req.file.size > newSize) {
+            // Replace the old image with the resized one
+            fs.unlink(fileRelativePath, (error) => {
+                if (error) {
+                    console.log("Original image could not be deleted (now trying to delete resized image)");
+                    console.error(error);
+                    fs.unlink(tmpFileRelativePath, (error) => {
+                        console.log("Resized image could not be deleted");
+                        console.error(error);
+                        throw error;
+                    });
+                    next();
+                    throw error;
+                } else {
+                    fs.rename(tmpFileRelativePath, fileRelativePath, (error) => {
+                        if (error) {
+                            console.log("Resized image could not be renamed");
+                            console.error(error);
+                            throw error;
+                        }
+                        next();
+                    });
+                }
+            });
+        } else {
+            // Delete the resized image and keep the original one
+            fs.unlink(tmpFileRelativePath, (error) => {
+                if (error) {
+                    console.log("Resized image could not be deleted");
+                    console.error(error);
+                    throw error;
+                }
+            });
+        }
+    } else {
+        // No need to resize the image
+        next();
+    }
+}
+
+export default resizeImage;
--- backend/routes/sauce.js
+++ backend/routes/sauce.js
@@ -2,14 +2,15 @@ import express from 'express';
 import auth from '../middlewares/auth.js';
 import authoriseForSauce from '../middlewares/auth-sauce.js';
 import multer from '../middlewares/multer-config.js';
+import resizeImage from '../middlewares/image-resize.js';
 import * as sauceCtrl  from '../controllers/sauce.js';
 
 const router = express.Router();
 
 router.get('/', auth, sauceCtrl.getAllSauces);
 router.get('/:id', auth, sauceCtrl.getOneSauce);
-router.post('/', auth, multer, sauceCtrl.createSauce);
-router.put('/:id', auth, authoriseForSauce, multer, sauceCtrl.modifySauce);
+router.post('/', auth, multer, resizeImage, sauceCtrl.createSauce);
+router.put('/:id', auth, authoriseForSauce, multer, resizeImage, sauceCtrl.modifySauce);
 router.delete('/:id', auth, authoriseForSauce, sauceCtrl.deleteSauce);
 router.post('/:id/like', auth, sauceCtrl.likeSauce);
 
