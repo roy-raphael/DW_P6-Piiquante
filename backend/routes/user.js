72e48b706259565ca9be4fca676b8f2840dc0413
Raphael Roy <roy.raphael.33@gmail.com>
2022-07-24 10:22:43 +0200

    feat: add password validator

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -3,8 +3,9 @@ import express from 'express';
 const router = express.Router();
 
 import * as userCtrl  from '../controllers/user.js';
+import validatePassword from '../middlewares/password-validator.js';
 
-router.post('/signup', userCtrl.signup);
+router.post('/signup', validatePassword, userCtrl.signup);
 router.post('/login', userCtrl.loginRateLimiter, userCtrl.login);
 
 export default router;
